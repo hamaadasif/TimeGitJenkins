ffc9854d42fdc7e6e1445413c0de46a30ef3318f
hamaa <xhamaad@gmail.com>
2024-03-06 09:12:35 -0500

    fixed issue

--- eclipse-workspace/Time/src/time/Time.java
+++ eclipse-workspace/Time/src/time/Time.java
@@ -1,7 +1,27 @@
+package time;
 
+import java.util.Scanner;
+
+/**
+ * @modfied by Liz Dancy
+ * Used to show how to catch multiple Exceptions and then as a base class
+ * for the testing exercise with JUnit
+ *
+ */
+
+
+import javax.swing.JOptionPane;
+
+/**
+ * Taken from Wendi Jollymore :http://www-acad.sheridanc.on.ca/~jollymor/prog24178/oop2.html
+ * @modfied by Liz Dancy
+ * Used  as a base class
+ * for the testing exercise with JUnit
+ *  Winter 2021
+ */
 public class Time {
-    public static void main(String[] args) {
-    	int totalSeconds = getTotalSeconds("10:10:10");
+	public static void main(String[] args) {
+    	double totalSeconds = getTotalSeconds("10:10:10");
     	System.out.println("Total Seconds = "+totalSeconds);
     }
 
@@ -14,9 +34,14 @@ public class Time {
         return hours * 3600 + minutes * 60 + seconds + (milliseconds / 1000.0);
     }
 
-    public static int getMilliseconds(String time) throws NumberFormatException, StringIndexOutOfBoundsException {
-        return Integer.parseInt(time.substring(9, 11));
+    public static int getMilliseconds(String time) throws NumberFormatException {
+        if (time.length() > 8) {
+            return Integer.parseInt(time.substring(9, 11));
+        } else {
+            return 0;
+        }
     }
+
 
     public static int getSeconds(String time) throws NumberFormatException, StringIndexOutOfBoundsException {
         return Integer.parseInt(time.substring(6, 8));
